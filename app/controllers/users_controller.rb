575096d218c094101d3c60d8c3d02352b4c7f1e5
Tech.Academy.291.5785 <techacademy.291.5785@gmail.com>
2021-06-15 07:52:23 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
@@ -22,6 +23,8 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
